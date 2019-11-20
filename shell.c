6df294780390becc32a873b228f920f160d07148
Jose David Caro <j.caro0207@gmail.com>
2019-11-20 22:47:15 +0000

    Adding changes of shell.c

--- shell.c
+++ shell.c
@@ -2,56 +2,78 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+
+/**
+* signalhandler - Ignore CTRL + C.
+*
+* Return: Nothing.
+*/
+void signalhandler()
+{
+       signal(SIGINT, signalhandler);
+       write(1, "\n$ ", 3);
+}
 
 int _strcmp(char *s1, char *s2)
 {
        int compare, i;
        for (i = 0; s2[i] != '\0'; i++)
        {
-               compare = s1[i] - s2[i];
+	       compare = s1[i] - s2[i];
                if (compare != 0)
                        break;
        }
        return (compare);
 }
+
 int  main ()
 {
 	char *buffer, *token, *salida;
-	size_t bufsize = 32;
+	size_t bufsize = 1024;
 	size_t characters;
-	char delimit[] = " \n";
 	char *argv[256];
-	int num_words, i, compare;
+	int num_words, i, f, a;
 
 	num_words = 0;
 	i = 0;
-	compare = 1;
 	salida = "exit\n";
 	buffer = malloc(bufsize * sizeof(char));
 	if (buffer == NULL)
 		return (0);
-	while (compare != 0)
+	while (1)
 	{
 		write(1, "$ ", 2);
+		signal(SIGINT, signalhandler);
 		characters = getline(&buffer,&bufsize,stdin);
-		compare = _strcmp(salida, buffer);
-		fork();
-		wait(NULL);
-		token = strtok(buffer, " \n");
-		while (token != NULL)
+		if (a = _strcmp(salida, buffer) == 0)
 		{
-			argv[i] = token;
-			printf("text: %s\n", argv[i]);
-			token = strtok(NULL, " \n");
-			i++;
+			free(buffer);
+			exit(98);
 		}
-		argv[i] = NULL;
-		execve(argv[0], argv, NULL);
+
+		if (fork() == 0)
+		{
+			token = strtok(buffer, " \n\t");
+			while (token != NULL)
+			{
+				argv[i] = token;
+				printf("text: %s\n", argv[i]);
+				token = strtok(NULL, " \n\t");
+				i++;
+			}
+			argv[i] = NULL;
+			execve(argv[0], argv, NULL);
+			for ( i = 0; argv[i] != NULL; i++ )
+				argv[i] = NULL;
+			exit(1);
+		}
+		else
+			wait(NULL);
 	}
-	/* while (token != NULL) */
-	/* { */
-	/* 	printf("%s", token); */
-	/* 	token = strtok(NULL, " "); */
-	/* } */
 	return (0);
 }
