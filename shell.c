7d462bfb6d8d3b913a1706db79510ca0e06b5d0b
Jose David Caro <j.caro0207@gmail.com>
2019-11-20 16:06:11 +0000

    changes shell.c

--- shell.c
+++ shell.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int  main ()
 {
 	char *buffer, *token;
 	size_t bufsize = 32;
 	size_t characters;
+	char *argv[] = {"", NULL};
+	int i;
 
+	i = 0;
 	buffer = malloc(bufsize * sizeof(char));
 	if (buffer == NULL)
 		return (0);
 	write(1, "$ ", 2);
 	characters = getline(&buffer,&bufsize,stdin);
-	token = strtok(buffer, " ");
-	while (token != NULL)
+	argv[0] = strtok(buffer, "\n");
+	execve(argv[0], argv, NULL);
+	while(argv[i] != NULL)
 	{
-		printf("%s\n", token);
-		token = strtok(NULL, " ");
+		printf("text: %s\n", argv[i]);
+		i++;
 	}
+	/* while (token != NULL) */
+	/* { */
+	/* 	printf("%s", token); */
+	/* 	token = strtok(NULL, " "); */
+	/* } */
 	return (0);
 }
